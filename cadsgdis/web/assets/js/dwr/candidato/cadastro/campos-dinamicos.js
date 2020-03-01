14a0ef00b09e195d50dcc4df86a3823f2b668ffe
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2020-03-01 16:55:49 -0400

    formulário dinâmico dwr/cadastro: campos dependestes de Força: Estado/OM(Nome e Abreviatura) dinâmicos

--- cadsgdis/web/assets/js/dwr/candidato/cadastro/campos-dinamicos.js
+++ cadsgdis/web/assets/js/dwr/candidato/cadastro/campos-dinamicos.js
@@ -13,7 +13,46 @@ function montaSelectDependenteForca(idForca){
     });
 }
 
-$("select[name=txtForca]").change(function(){
+function montaSelectDependenteEstadoForca(idForca, idEstado){
+    FacadeAjax.getOmsByForcaAndEstado(idForca, idEstado, {
+       callback: function(list){
+           dwr.util.removeAllOptions("txtNomeOM");
+           dwr.util.addOptions("txtNomeOM", [{id: "0", nome: "Selecione uma OM..."}], "id", "nome");
+           dwr.util.addOptions("txtNomeOM", list, "id", "nome");
+       } 
+    });
+}
+function montaSelectDependenteOM(idOM){
+    FacadeAjax.getAbreviaturaOM(idOM, {
+       callback: function(abreviatura){
+           dwr.util.setValues({txtAbrevOM: abreviatura});
+       } 
+    });
+}
+
+$("select[name=txtForca]").change(function(){    
+    $("select[name=txtEstadoForca]").val("0");
+    $('select[name=txtEstadoForca] option:not(:selected)').remove();
+    
+    $("select[name=txtNomeOM]").val("0");
+    $('select[name=txtNomeOM] option:not(:selected)').remove();
+    
+    $("input[name=txtAbrevOM]").val('');
     montaSelectDependenteForca(this.value);
+    
+});
+
+$("select[name=txtEstadoForca]").change(function(){
+    $("select[name=txtNomeOM]").val("0");
+    $('select[name=txtNomeOM] option:not(:selected)').remove();
+    
+    $("input[name=txtAbrevOM]").val('');
+    var idForca = $("select[name=txtForca]").val();
+    montaSelectDependenteEstadoForca(idForca, this.value);
+    
+});
+
+$("select[name=txtNomeOM]").change(function(){
+    montaSelectDependenteOM(this.value);
 });
 
