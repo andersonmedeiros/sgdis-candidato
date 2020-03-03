c7b543328daae6326e3ed5efc16c86f35af5af28
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2020-03-03 16:44:40 -0400

    sql inserts

---
+++ bancodedados/sql/insert/insert_aluno.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM sgdis.Aluno
+LIMIT 0, 1000
+
+-- Date: 2020-03-03 09:05
+*/
+INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`) VALUES ('1207153370','AQUELE','AQUELE',9,1);
+INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`) VALUES ('1207153371','CICLANO','CICLANO',29,2);
+INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`) VALUES ('1207153378','FULANO DE TAL','DE TAL',34,2);
+INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`) VALUES ('1207153379','TESTE','TESTE',16,1);
--- bancodedados/sql/insert/insert_postograduacao.sql
+++ bancodedados/sql/insert/insert_postograduacao.sql
@@ -1,1 +1,78 @@
-INSERT INTO `sgdis`.`PostoGraduacao` (`id`, `nome`, `abreviatura`, `categoria`, `circulo`, `idForca`) VALUES ('1', 'PRIMEIRO TENENTE', '1º TEN', 'OFICIAL', 'OFSUBALT', '1');
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('ALMIRANTE','ALTE','OFICIAL','OFGEN',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('ALMIRANTE DE ESQUADRA','ALTE ESQ','OFICIAL','OFGEN',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('VICE ALMIRANTE','V ALTE','OFICIAL','OFGEN',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CONTRA ALMIRANTE','C ALTE','OFICIAL','OFGEN',1);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CAPITÃO DE MAR E GUERRA','CMG','OFICIAL','OFSUPE',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CAPITÃO DE FRAGATA','CF','OFICIAL','OFSUPE',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CAPITÃO DE CORVETA','CC','OFICIAL','OFSUPE',1);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CAPITÃO TENENTE','CT','OFICIAL','OFITR',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('PRIMEIRO TENENTE','1º TEN','OFICIAL','OFSUBLT',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SEGUNDO TENENTE','2º TEN','OFICIAL','OFSUBLT',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('GUARDA MARINHA','GM','PRAÇA ESPECIAL','OFSUBLT',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('ASPIRANTE','ASP','PRAÇA ESPECIAL','OFSUBLT',1);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SUBOFICIAL','SO','PRAÇA','SO_STEN_SGT',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('PRIMEIRO SARGENTO','1º SG','PRAÇA','SO_STEN_SGT',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SEGUNDO SARGENTO','2º SG','PRAÇA','SO_STEN_SGT',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TERCEIRO SARGENTO','3º SG','PRAÇA','SO_STEN_SGT',1);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CABO','CB','PRAÇA','CB_SD',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SOLDADO(CFN)','SD','PRAÇA','CB_SD',1);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('MARINHEIRO','MN','PRAÇA','CB_SD',1);
+
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('MARECHAL','MAR','OFICIAL','OFGEN',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('GENERAL DE EXÉRCITO','GEN EX','OFICIAL','OFGEN',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('GENERAL DE DIVISÃO','GEN DIV','OFICIAL','OFGEN',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('GENERAL DE BRIGADA','GEN BDA','OFICIAL','OFGEN',2);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CORONEL','CEL','OFICIAL','OFSUPE',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TENENTE CORONEL','TEN CEL','OFICIAL','OFSUPE',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('MAJOR','MAJ','OFICIAL','OFSUPE',2);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CAPITÃO','CAP','OFICIAL','OFITR',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('PRIMEIRO TENENTE','1º TEN','OFICIAL','OFSUBLT',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SEGUNDO TENENTE','2º TEN','OFICIAL','OFSUBLT',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('ASPIRANTE A OFICIAL','ASP','PRAÇA ESPECIAL','OFSUBLT',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CADETE','CAD','PRAÇA ESPECIAL','OFSUBLT',2);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SUBTENENTE','ST','PRAÇA','SO_STEN_SGT',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('PRIMEIRO SARGENTO','1º SGT','PRAÇA','SO_STEN_SGT',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SEGUNDO SARGENTO','2º SGT','PRAÇA','SO_STEN_SGT',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TERCEIRO SARGENTO','3º SGT','PRAÇA','SO_STEN_SGT',2);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CABO','CB','PRAÇA','CB_SD',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SOLDADO','SD','PRAÇA','CB_SD',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TAIFEIRO MOR','TM','PRAÇA','CB_SD',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TAIFEIRO DE PRIMEIRA CLASSE','T1','PRAÇA','CB_SD',2);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TAIFEIRO DE SEGUNDA CLASSE','T2','PRAÇA','CB_SD',2);
+
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('MARECHAL DO AR','MAR AR','OFICIAL','OFGEN',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TENENTE BRIGADEIRO','TEN BRIG','OFICIAL','OFGEN',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('MAJOR BRIGADEIRO','MAJ BRIG','OFICIAL','OFGEN',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('BRIGADEIRO','BRIG','OFICIAL','OFGEN',3);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CORONEL','CEL','OFICIAL','OFSUPE',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TENENTE CORONEL','TEN CEL','OFICIAL','OFSUPE',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('MAJOR','MAJ','OFICIAL','OFSUPE',3);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CAPITÃO','CAP','OFICIAL','OFITR',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('PRIMEIRO TENENTE','1º TEN','OFICIAL','OFSUBLT',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SEGUNDO TENENTE','2º TEN','OFICIAL','OFSUBLT',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('ASPIRANTE A OFICIAL','ASP','PRAÇA ESPECIAL','OFSUBLT',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CADETE','CAD','PRAÇA ESPECIAL','OFSUBLT',3);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SUBOFICIAL','SO','PRAÇA','SO_STEN_SGT',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('PRIMEIRO SARGENTO','1S','PRAÇA','SO_STEN_SGT',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SEGUNDO SARGENTO','2S','PRAÇA','SO_STEN_SGT',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TERCEIRO SARGENTO','3S','PRAÇA','SO_STEN_SGT',3);
+
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('CABO','CB','PRAÇA','CB_SD',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SOLDADO DE PRIMEIRA CLASSE','S1','PRAÇA','CB_SD',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('SOLDADO DE SEGUNDA CLASSE','S2','PRAÇA','CB_SD',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TAIFEIRO MOR','TM','PRAÇA','CB_SD',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TAIFEIRO DE PRIMEIRA CLASSE','T1','PRAÇA','CB_SD',3);
+INSERT INTO `PostoGraduacao` (`nome`,`abreviatura`,`categoria`,`circulo`,`idForca`) VALUES ('TAIFEIRO DE SEGUNDA CLASSE','T2','PRAÇA','CB_SD',3);
