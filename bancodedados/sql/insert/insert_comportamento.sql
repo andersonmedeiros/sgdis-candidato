122f1a756965b17f4fffc64a2e707d8964cd8d1f
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2020-03-09 14:33:43 -0400

    sql inserts

---
+++ bancodedados/sql/insert/insert_comportamento.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM sgdis.Comportamento
+LIMIT 0, 1000
+
+-- Date: 2020-03-09 14:08
+*/
+INSERT INTO `Comportamento` (`id`,`nome`) VALUES (1,'EXCEPCIONAL');
+INSERT INTO `Comportamento` (`id`,`nome`) VALUES (2,'ÓTIMO');
+INSERT INTO `Comportamento` (`id`,`nome`) VALUES (3,'BOM');
+INSERT INTO `Comportamento` (`id`,`nome`) VALUES (4,'INSUFICIENTE');
+INSERT INTO `Comportamento` (`id`,`nome`) VALUES (5,'MAU');
