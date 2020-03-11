90d8d0442c51da92071c06e78da9b3762f017486
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2020-03-11 15:54:27 -0400

    sql inserts

--- bancodedados/sql/insert/insert_grauparentesco.sql
+++ bancodedados/sql/insert/insert_grauparentesco.sql
@@ -1,14 +1,20 @@
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (1,'Avô');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (2,'Avó');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (3,'Pai');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (4,'Mãe');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (5,'Irmão');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (6,'Irmã');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (7,'Tio');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (8,'Tia');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (9,'Filho');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (10,'Filha');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (11,'Primo');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (12,'Prima');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (13,'Sogro');
-INSERT INTO `sgdis`.`GrauParentesco` (`id`,`nome`) VALUES (14,'Sogra');
+/*
+-- Query: SELECT * FROM sgdis.GrauParentesco
+LIMIT 0, 1000
+
+-- Date: 2020-03-11 11:20
+*/
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (1,'AVÔ');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (2,'AVÓ');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (3,'PAI');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (4,'MÃE');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (5,'IRMÃO');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (6,'IRMÃ');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (7,'TIO');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (8,'TIA');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (9,'FILHO');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (10,'FILHA');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (11,'PRIMO');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (12,'PRIMA');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (13,'SOGRO');
+INSERT INTO `GrauParentesco` (`id`,`nome`) VALUES (14,'SOGRA');
