1412dd46a9c8e56821bc0d70fa6bb04188cd4346
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2020-03-16 16:55:36 -0400

    sql inserts

--- bancodedados/sql/insert/insert_aluno.sql
+++ bancodedados/sql/insert/insert_aluno.sql
@@ -4,7 +4,7 @@ LIMIT 0, 1000
 
 -- Date: 2020-03-03 09:05
 */
-INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`, `idQasQms`, `idEstadoCivil`, `idComportamento`) VALUES ('1207153370','AQUELE','AQUELE',6,1,1,1,2);
-INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`, `idQasQms`, `idEstadoCivil`, `idComportamento`) VALUES ('1207153371','CICLANO','CICLANO',10,1,2,2,2);
-INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`, `idQasQms`, `idEstadoCivil`, `idComportamento`) VALUES ('1207153378','FULANO DE TAL','DE TAL',11,1,3,2,3);
-INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`, `idQasQms`, `idEstadoCivil`, `idComportamento`) VALUES ('1207153379','TESTE','TESTE',12,1,4,3,1);
+INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`, `idQasQms`, `idEstadoCivil`, `idComportamento`) VALUES ('1207153370','AQUELE','AQUELE',91,1,1,1,2);
+INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`, `idQasQms`, `idEstadoCivil`, `idComportamento`) VALUES ('1207153371','CICLANO','CICLANO',90,2,2,2,2);
+INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`, `idQasQms`, `idEstadoCivil`, `idComportamento`) VALUES ('1207153378','FULANO DE TAL','DE TAL',96,2,3,2,3);
+INSERT INTO `Aluno` (`identidade`,`nome`,`nomeguerra`,`idPostoGraduacao`,`idOM`, `idQasQms`, `idEstadoCivil`, `idComportamento`) VALUES ('1207153379','TESTE','TESTE',98,1,4,3,1);
