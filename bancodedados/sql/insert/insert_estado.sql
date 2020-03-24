722a62b50d9f5a84f89cbb43df1e5e7faff79371
Anderson de Paula Andrade Medeiros <andersonandrade.30@gmail.com>
2020-03-24 08:51:19 -0400

    sql inserts

--- bancodedados/sql/insert/insert_estado.sql
+++ bancodedados/sql/insert/insert_estado.sql
@@ -1,4 +1,33 @@
-INSERT INTO `sgdis`.`Estado` (`nome`, `sigla`) VALUES ('AMAZONAS', 'AM');
-INSERT INTO `sgdis`.`Estado` (`nome`, `sigla`) VALUES ('ACRE', 'AC');
-INSERT INTO `sgdis`.`Estado` (`nome`, `sigla`) VALUES ('SÃO PAULO', 'SP');
-INSERT INTO `sgdis`.`Estado` (`nome`, `sigla`) VALUES ('RIO DE JANEIRO', 'RJ');
+/*
+-- Query: SELECT * FROM sgdis.Estado
+LIMIT 0, 1000
+
+-- Date: 2020-03-24 08:50
+*/
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (1,'ACRE','AC');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (2,'ALAGOAS','AL');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (3,'AMAZONAS','AM');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (4,'AMAPÁ','AP');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (5,'BAHIA','BA');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (6,'CEARÁ','CE');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (7,'DISTRITO FEDERAL','DF');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (8,'ESPÍRITO SANTO','ES');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (9,'GOIÁS','GO');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (10,'MARANHÃO','MA');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (11,'MINAS GERAIS','MG');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (12,'MATO GROSSO DO SUL','MS');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (13,'MATO GROSSO','MT');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (14,'PARÁ','PA');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (15,'PARAÍBA','PB');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (16,'PERNAMBUCO','PE');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (17,'PIAUÍ','PI');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (18,'PARANÁ','PR');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (19,'RIO DE JANEIRO','RJ');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (20,'RIO GRANDE DO NORTE','RN');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (21,'RONDÔNIA','RO');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (22,'RORAIMA','RR');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (23,'RIO GRANDE DO SUL','RS');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (24,'SANTA CATARINA','SC');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (25,'SERGIPE','SE');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (26,'SÃO PAULO','SP');
+INSERT INTO `Estado` (`id`,`nome`,`sigla`) VALUES (27,'TOCANTINS','TO');
